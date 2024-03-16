26fdcdc2a533188de53dc26a62a0b06b0b9f8480
diego-sierra123 <pruebitaapruebitaa805@gmail.com>
2024-03-16 09:21:14 -0500

    first commit

---
+++ importante/src/componentes/Vehiculosview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import vehiculos from "./vehiculos";
+
+function Vehiculosview({vehiculos}){
+    return(
+        <div>
+            <h1>{vehiculos.name}</h1>
+            <h2>{vehiculos.description}</h2>
+            <h2>Diego addsbhdd</h2>
+        </div>
+    )
+}
+
+export default Vehiculosview;
